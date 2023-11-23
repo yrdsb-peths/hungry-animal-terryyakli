cd5a0336d172390ad195f8ec0dd08a3678ba1d4f
Theresa <ylitheresa@gmail.com>
2023-11-23 12:34:37 -0500

    Version 1.00

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private int random;
+    private int counter = 0; 
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +17,37 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1); 
+        Wombat wombat = new Wombat();
+        addObject(wombat, 300, 300);
+        
     }
+    public void act(){
+        if(counter == 0){
+            spawnApple();
+            counter = 30; 
+        }
+        else{
+            counter--; 
+        }
+    }
+
+    public void spawnApple() {
+            random = Greenfoot.getRandomNumber(2); 
+            Apple apple = new Apple();
+            if(random == 0){
+                addObject(apple, 100, 0);
+            }
+            if(random == 1){
+                addObject(apple, 200, 0);
+            }
+            //write for >500
+            if(random == 2){
+                addObject(apple, 500, 0); 
+            }
+        
+    }
+    
+    
 }
+
---
+++ Wombat.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Wombat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Wombat extends Actor
+{
+    /**
+     * Act - do whatever the Wombat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        keys(); 
+        checkApple();
+    }
+    public void keys(){
+        if(Greenfoot.isKeyDown("a")){
+            move(-5); 
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(5); 
+        }
+        
+        
+        //write code for how to move left and right
+        
+    }
+    public void checkApple(){
+        Apple a = (Apple)getOneIntersectingObject(Apple.class); 
+        if(a != null){
+            getWorld().removeObject(a); 
+        }
+    }
+}
