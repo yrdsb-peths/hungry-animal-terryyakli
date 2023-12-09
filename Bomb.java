e6ce8888bde5b643b18a4124709430199c9d2112
Theresa <ylitheresa@gmail.com>
2023-12-09 13:03:58 -0500

    Made bomb

---
+++ Bomb.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Bomb will end the game.
+ * 
+ * @author Theresa Li 
+ * @version December 2023
+ */
+public class Bomb extends Actor
+{
+    int speed = 1;
+    
+    public Bomb()
+    {
+        GreenfootImage bomb = new GreenfootImage("bomb.png");
+        
+    }
+    public void act()
+    {
+        // Add your action code here.
+        
+    }
+}
